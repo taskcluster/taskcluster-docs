a7fb648348edb0ffa2b4300214c617504b380278
Dustin J. Mitchell <dustin@mozilla.com>
2016-03-25 21:59:52 +0000

    Bug 1232018: un-taint hash before handing it to jQuery

--- assets/docref/docref.js
+++ assets/docref/docref.js
@@ -21,7 +21,8 @@ $(function() {
       // Wait 250ms and scroll to anchor after loading
       setTimeout(function() {
         var hash = (window.location.hash || "").substr(1);
-        if(hash && hash !== "") {
+        // be a little careful about what gets interpolated into the jQuery here
+        if(hash && hash.match(/^[a-zA-Z0-9_ -]+$/)) {
           var anchor = $("a[name='"+ hash +"']");
           if (anchor.size() != 0) {
             $('html,body').scrollTop(anchor.offset().top);
