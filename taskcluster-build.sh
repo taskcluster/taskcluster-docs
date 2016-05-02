7ff1009820ff67c52c0ba6a06f05bc5afd2f5cdf
Dustin J. Mitchell <dustin@mozilla.com>
2016-05-02 13:42:24 +0000

    add a script to run 'gulp check' on PRs

--- taskcluster-build.sh
+++ taskcluster-build.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-echo "building:" $1
+set -e -x
+
+npm install --quiet
+export PATH="$PWD/node_modules/.bin:$PATH"
+
+if [ "$1" = "pull-request" ]; then
+    gulp check
+else
+    # TODO: get S3 credentials
+    export PUBLISH_BUCKET=docs.taskcluster.net
+    gulp publish
+fi
