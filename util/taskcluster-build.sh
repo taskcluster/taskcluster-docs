6d6c1582582bd71301ceb186a94111c10bb318e0
Dustin J. Mitchell <dustin@mozilla.com>
2016-05-04 21:07:55 +0000

    use the correct bucket

--- util/taskcluster-build.sh
+++ util/taskcluster-build.sh
@@ -8,8 +8,8 @@ export PATH="$PWD/node_modules/.bin:$PATH"
 if [ "$1" = "pull-request" ]; then
     gulp check
 elif [ "$1" = "push" ]; then
-    export PUBLISH_BUCKET=docs-taskcluster-net2
-    export PUBLISH_REGION=us-east-1
+    export PUBLISH_BUCKET=docs-taskcluster-net
+    export PUBLISH_REGION=us-west-2
     eval $(node util/get-sts-credentials.js $PUBLISH_BUCKET)
     gulp publish
 fi
